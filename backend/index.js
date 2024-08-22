1b847a7e9fcc3befc9bccd35a7f3567c6ba35555
Zain-128 <100517579+Zain-128@users.noreply.github.com>
2024-08-21 18:27:40 -0700

    Connected to Mongo db

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import { ConnectToMongo } from "./config/db.connect.js";
+import dotenv from "dotenv";
 
+dotenv.config({});
 const app = express();
 
 app.listen(process.env.PORT, () => {
+  ConnectToMongo();
   console.log(`Server is Running ! `);
 });
